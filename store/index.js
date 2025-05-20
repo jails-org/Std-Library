40e91eb40265df0fac5d42af9927ed677735f8d6
Javiani <eduardo.ottaviani@gmail.com>
2025-05-20 11:47:28 -0300

    Syncing Onijs version

--- store/index.js
+++ store/index.js
@@ -1,33 +1,40 @@
 function S(e, d) {
   let r = [];
-  const l = /* @__PURE__ */ new Set(), c = w(e), p = () => c, s = (o) => {
-    if (o.call)
-      return l.add(o), () => {
-        l.delete(o);
+  const l = /* @__PURE__ */ new Set(), c = g(e), p = () => c, s = (a) => {
+    if (a.call)
+      return l.add(a), () => {
+        l.delete(a);
       };
     {
-      const n = (t, { action: a, payload: i }) => {
-        a in o && o[a].call(null, t, { action: a, payload: i });
+      const n = (t, { action: o, payload: i }) => {
+        o in a && a[o].call(null, t, { action: o, payload: i });
       };
-      return () => {
+      return l.add(n), () => {
         l.delete(n);
       };
     }
-  }, u = (o, n) => (r.push({ action: o, payload: n }), new Promise((t) => f((a) => g({ action: o, payload: n }, t)))), y = (o) => new Promise((n) => {
-    s((t, { action: a, payload: i }) => {
-      a in o && f((h) => {
-        o[a].call(null, t, { action: a, payload: i }), n(t);
+  }, u = (a, n) => (r.push({ action: a, payload: n }), new Promise(
+    (t) => f({ action: a, payload: n }, t)
+  )), y = (a) => new Promise((n) => {
+    s((t, { action: o, payload: i }) => {
+      o in a && w((h) => {
+        a[o].call(null, t, { action: o, payload: i }), n(t);
       });
     });
-  }), g = ({ action: o, payload: n = {} }, t) => {
-    r.forEach(({ action: a, payload: i = {} }) => {
-      if (!(a in d))
-        console.log(`[Oni] Error -> No action [ ${a} ] found.`);
+  }), f = ({ action: a, payload: n = {} }, t) => {
+    r.forEach(({ action: o, payload: i = {} }) => {
+      if (!(o in d))
+        console.log(`[Oni] Error -> No action [ ${o} ] found.`);
       else {
-        const h = d[a].call(null, c, i, { getState: p, subscribe: s, dispatch: u, patternMatch: y });
+        const h = d[o].call(null, c, i, {
+          getState: p,
+          subscribe: s,
+          dispatch: u,
+          patternMatch: y
+        });
         Object.assign(c, h);
       }
-    }), r.length && (l.forEach((a) => a(c, { action: o, payload: n })), r = []), t(c);
+    }), r.length && (l.forEach((o) => o(c, { action: a, payload: n })), r = []), t(c);
   };
   return {
     getState: p,
@@ -37,7 +44,7 @@ function S(e, d) {
     destroy: () => l.clear()
   };
 }
-const w = (e) => JSON.parse(JSON.stringify(e)), f = typeof window > "u" ? (e) => e() : (e) => requestAnimationFrame(e);
+const g = (e) => JSON.parse(JSON.stringify(e)), w = typeof window > "u" ? (e) => e() : (e) => requestAnimationFrame(e);
 export {
   S as Store
 };
